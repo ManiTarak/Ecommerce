7edb26564484d73bfac48b2665c41e1adcb913d0
Mani Jammula <manikumarjammula999@gmail.com>
2024-05-17 11:13:31 +0530

    responsive navbar and footer added using tailwind css

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,8 +1,22 @@
+import { Link } from "react-router-dom";
+
 function Footer() {
   return (
-    <div>
-      <h1>This is the footer section</h1>
-      <h1>This is the footer section</h1>
+    <div className="bg-neutral-900 flex  flex-col justify-center items-center text-white">
+      <h2 className="text-2xl mt-4 font-semibold">
+        All Rights Reserved by &copy; Mani MJ
+      </h2>
+      <div className="flex gap-4 mt-2 text-sm mb-1">
+        <Link className="hover:text-gray-400" to="/about">
+          About &nbsp; |
+        </Link>
+        <Link className="hover:text-gray-400" to="/contact">
+          Contact &nbsp; |
+        </Link>
+        <Link className="hover:text-gray-400" to="/policy">
+          Privacy Policy &nbsp;
+        </Link>
+      </div>
     </div>
   );
 }
--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,5 +1,71 @@
+import { Link } from "react-router-dom";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { FaTimes } from "react-icons/fa";
+import { useState } from "react";
+
 function Header() {
-  return <h1>This is the Header Component</h1>;
+  const [nav, setNav] = useState(false);
+  const handleClick = () => {
+    setNav(!nav);
+  };
+  return (
+    <>
+      <nav className="sticky top-0 h-16 left-0 flex justify-between items-center bg-[#131921]  ">
+        <h1 className="text-white font-bold text-4xl ml-5">Website</h1>
+
+        <ul className="hidden text-white md:flex gap-6 mr-5 text-lg">
+          <Link className="hover:text-gray-400 hover:underline" to="/">
+            <li>Home</li>
+          </Link>
+
+          <Link className="hover:text-gray-400 hover:underline" to="/contact">
+            <li>Contact</li>
+          </Link>
+
+          <Link className="hover:text-gray-400 hover:underline" to="/register">
+            <li>Register</li>
+          </Link>
+
+          <Link className="hover:text-gray-400 hover:underline" to="/login">
+            <li>Login</li>
+          </Link>
+        </ul>
+        <div className="md:hidden mr-4 z-10" onClick={handleClick}>
+          {nav ? (
+            <FaTimes size={25} color="white" />
+          ) : (
+            <RxHamburgerMenu size={25} color="white" />
+          )}
+        </div>
+        <ul
+          className={`${
+            nav
+              ? "text-white opacity-100 transform translate-x-0"
+              : "text-gray opacity-0 transform -translate-y-full"
+          } transition-transform absolute top-16 left-0 w-full h- flex flex-col justify-around items-center text-xl bg-zinc-800/80 md:hidden`}
+          onClick={() => {
+            setNav(false);
+          }}
+        >
+          <Link className="hover:text-gray-400" to="/">
+            <li>Home</li>
+          </Link>
+
+          <Link className="hover:text-gray-400" to="/contact">
+            <li>Contact</li>
+          </Link>
+
+          <Link className="hover:text-gray-400" to="/register">
+            <li>Register</li>
+          </Link>
+
+          <Link className="hover:text-gray-400" to="/login">
+            <li>Login</li>
+          </Link>
+        </ul>
+      </nav>
+    </>
+  );
 }
 
 export default Header;
