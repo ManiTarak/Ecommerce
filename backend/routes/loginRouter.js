702501e2606ecb36fb48570aa98fd0e07d97235d
Mani Jammula <manikumarjammula999@gmail.com>
2024-06-23 19:07:09 +0530

    added user profile page and enabled editing profile details and maintained separate route in backend to update profile details

--- backend/routes/loginRouter.js
+++ backend/routes/loginRouter.js
@@ -3,6 +3,8 @@ const loginRouter = express.Router();
 const jwt = require("jsonwebtoken");
 const loginValidator = require("../middlewares/loginValidator.js");
 const authenticationCheck = require("../middlewares/authMiddleware.js");
+const User = require("../db/user.js");
+const { hashpassword } = require("../helpers/hashingPass.js");
 // LOGIN || POST
 loginRouter.post("/", loginValidator, (req, res) => {
   const userId = req.id;
@@ -36,6 +38,49 @@ loginRouter.post("/", loginValidator, (req, res) => {
 
 loginRouter.get("/test", authenticationCheck, (req, res) => {
   res.send("<h1>This sample protected route</h1>");
+});
+
+// Profile Details
+loginRouter.put("/update-profile", authenticationCheck, async (req, res) => {
+  try {
+    const { name, password, email, phone, address, sport } = await User.findOne(
+      {
+        _id: req.id,
+      }
+    );
+    if (req.body.password) {
+      var hashedPassword = await hashpassword(req.body.password);
+    }
+    await User.findByIdAndUpdate(req.id, {
+      _id: req.id,
+      name: req.body.name || name,
+      email: req.body.email || email,
+      password: hashedPassword || password,
+      phone: req.body.phone || phone,
+      address: req.body.address || address,
+      role: 1,
+    });
+    const userDetails = await User.findOne({ _id: req.id });
+    res.status(200).send({
+      success: true,
+      user: {
+        id: userDetails._id,
+        name: userDetails.name,
+        email: userDetails.email,
+        phone: userDetails.phone,
+        address: userDetails.address,
+        sport: userDetails.sport,
+        role: userDetails.role,
+      },
+      message: "User Profile Details Updated successfully",
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).send({
+      success: false,
+      message: "Something bad happend while updating products details",
+    });
+  }
 });
 
 module.exports = loginRouter;
--- frontend/src/pages/User/Profile.js
+++ frontend/src/pages/User/Profile.js
@@ -1,19 +1,125 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth } from "../../context/auth";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const Profile = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [sport, setSport] = useState("");
   const [auth, setAuth] = useAuth();
+  useEffect(() => {
+    if (auth?.user) {
+      setName(auth.user.name);
+      setEmail(auth.user.email);
+      setPhone(auth.user.phone);
+      setAddress(auth.user.address);
+    }
+  }, []);
+
+  // function to update Profile of user
+  const handleUpdateDetails = async (e) => {
+    e.preventDefault();
+    try {
+      const result = await axios.put(
+        `${process.env.REACT_APP_URL}/login/update-profile`,
+        {
+          name: name,
+          email: email,
+          password: password,
+          phone: phone,
+          address: address,
+          sport: sport,
+        },
+        {
+          headers: {
+            Authorization: auth?.token,
+          },
+        }
+      );
+      const authInfo = JSON.parse(localStorage.getItem("authInfo"));
+      authInfo.user = result.data.user;
+      setAuth(authInfo);
+      localStorage.setItem("authInfo", JSON.stringify(authInfo));
+      toast.success(result.data.message);
+    } catch (e) {
+      toast.error(e.response.data.message);
+    }
+  };
   return (
-    <div className="flex flex-col w-[100%] h-[100%] justify-center ">
-      <div className="p-[10px] m-[10px] text-4xl font-serif text-gray-100 font-medium">
-        {"User Name:" + auth?.user?.name}
-      </div>
-      <div className="p-[10px] m-[10px] text-4xl font-serif text-gray-100 font-medium">
-        {"User Email:" + auth?.user?.email}
-      </div>
-      <div className="p-[10px] m-[10px] text-4xl font-serif text-gray-100 font-medium">
-        {"User Phone:" + auth?.user?.phone}
-      </div>
+    <div className="flex justify-center items-center w-full h-full min-h-[77.7vh] bg-gradient-to-r from-green-400 to-blue-500">
+      <form
+        onSubmit={handleUpdateDetails}
+        className="flex flex-col  w-96 justify-center bg-yellow-300 items-center bg-gradient-to-r from-pink-500 to-yellow-500 shadow-md shadow-green-700"
+      >
+        <h1 className="text-center mb-5 mt-2 font-bold text-black-500 text-3xl font-serif">
+          PROFILE
+        </h1>
+        <input
+          className="border-b-2 text-center border-solid border-slate-500 h-8 w-52 mb-2 focus:outline-none focus:ring-0 focus:border-gray-900"
+          placeholder="Enter Name"
+          type="text"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        ></input>
+        <input
+          className="border-b-2 text-center border-solid border-slate-500 h-8  w-52 mb-2 focus:outline-none focus:ring-0 focus:border-gray-900"
+          placeholder="Enter Email"
+          type="email"
+          value={email}
+          disabled
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        ></input>
+        <input
+          className="border-b-2 text-center border-solid border-slate-500 h-8  w-52 mb-2 focus:outline-none focus:ring-0 focus:border-gray-900"
+          placeholder="Enter Password"
+          type="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        ></input>
+        <input
+          className="border-b-2 text-center border-solid border-slate-500 h-8  w-52 mb-2 focus:outline-none focus:ring-0 focus:border-gray-900"
+          placeholder="Enter PhoneNo"
+          type="number"
+          value={phone}
+          onChange={(e) => {
+            setPhone(e.target.value);
+          }}
+        ></input>
+        <input
+          className="border-b-2 text-center border-solid border-slate-500 h-8  w-52 mb-2 focus:outline-none focus:ring-0 focus:border-gray-900"
+          placeholder="Enter Your Faviourte Sport"
+          type="text"
+          value={sport}
+          onChange={(e) => {
+            setSport(e.target.value);
+          }}
+        ></input>
+        <input
+          className="border-b-2 text-center border-solid border-slate-500 h-8  w-52 mb-2 focus:outline-none focus:ring-0 focus:border-gray-900"
+          placeholder="Enter Address"
+          type="text"
+          value={address}
+          onChange={(e) => {
+            setAddress(e.target.value);
+          }}
+        ></input>
+        <button
+          type="submit"
+          className="text-white text-xl mt-5 mb-2 font-semibold bg-black py-2 px-8 w-52 font-serif "
+        >
+          Update
+        </button>
+      </form>
     </div>
   );
 };
